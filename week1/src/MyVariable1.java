873df6a6947c2f331da9cef829933e812adb8601
AdamKolonics88 <adam.kolonics@gmail.com>
2025-04-23 21:06:45 +0200

    Add files via upload

---
+++ week1/src/MyVariable1.java
@@ -0,0 +1,20 @@
+public class MyVariable1 {
+    public static void main(String[] args) {
+        String name = "Ádám";
+        int hours = 40;
+        System.out.println(name);
+        System.out.println(hours);
+
+        String building = "Empire State", tree = "Maple", lake = "Lake Superior", ocean = "Pacific Ocean";
+        System.out.println(tree);
+        System.out.println(building);
+        System.out.println(lake);
+        System.out.println(ocean);
+
+        int weeks = 4, decades = 10, months = 3, years = 5;
+        System.out.println(weeks);
+        System.out.println(decades);
+        System.out.println(months);
+        System.out.println(years);
+    }
+}
